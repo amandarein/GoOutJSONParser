75e6f81e2441bbec53fd5d9ab0698d13abdeca8d
amandarein <46791101+amandarein@users.noreply.github.com>
2019-04-29 13:13:16 -0400

    Add files via upload

--- GoOutJSONReader/src/main.java
+++ GoOutJSONReader/src/main.java
@@ -19,28 +19,29 @@ import java.util.Set;
 
 import org.json.JSONObject;
 
-
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 public class main {
 
 	public static void main(String[] args) throws Exception {
 		
 		FileReader jsonFile = new FileReader("data/events.json");
+		
 		JsonParser jsonParser = new JsonParser();
 		String jsonString = new String(Files.readAllBytes(Paths.get("data/events.json")));
 		jsonString = jsonString.replaceAll("[\r\n]+", "");
 		jsonString = jsonString.replaceAll("      ", "");
+		jsonString = jsonString.replaceAll("  ", "");
 		
 		String scheduleString = jsonString.substring(jsonString.indexOf("\"schedule\": [")+13, jsonString.indexOf("\"venues\":"));
 		String venueString = jsonString.substring(jsonString.indexOf("\"venues\":"), jsonString.indexOf("\"events\":"));
 		String eventString = jsonString.substring(jsonString.indexOf("\"events\":"));
-		System.out.println(scheduleString);
-		System.out.println(venueString);
-		System.out.println(eventString);
 		
 		Gson gson = new GsonBuilder()
 		        .setLenient()
 		        .create();
 		
+		/*//how we would get each schedule element individually as a string
 		ArrayList<String> events = new ArrayList<String>();
 		int indEnd = 0;
 		for(int i = 0; i < 1000; i++) {
@@ -50,12 +51,9 @@ public class main {
 				scheduleString = scheduleString.substring(indEnd);
 				System.out.println(events.get(i));
 			}
-		}
+		} */
 		
-		//look at https://github.com/google/gson/blob/master/UserGuide.md
-		for(int j = 0; j < events.size(); j++) {
-			Event e = gson.fromJson(events.get(j), Event.class);
-			System.out.println(e.toString());
-		}
+		java.lang.reflect.Type itemListType = new TypeToken<ArrayList<Event>>(){}.getType();
+		ArrayList<Event> itemList = gson.fromJson(scheduleString, itemListType);
 		} 
 }
